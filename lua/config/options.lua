34782021b23e7fe305cbbecd0c78a338716dccaa
Xirlorm <xirlorm@gmail.com>
2026-05-07 08:06:56 +0000

    Changed editor character width guide lines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,15 +2,15 @@ local set = vim.opt
 local g = vim.g
 
 set.compatible = false --Enable vim features unavailable in vi
-vim.cmd('filetype on') --Enable filetype detection
-vim.cmd('filetype plugin on') --Load filetype specific plugins
-vim.cmd('filetype indent on') --Enable indentation based on file type
+vim.cmd("filetype on") --Enable filetype detection
+vim.cmd("filetype plugin on") --Load filetype specific plugins
+vim.cmd("filetype indent on") --Enable indentation based on file type
 vim.cmd("syntax on") -- Enable syntax highlighting
 
 --  Editor variables
 set.number = true -- Enable line numbering
 set.relativenumber = false -- Disalbe line relative line numbering
-set.mouse = 'a' -- Enable mouse
+set.mouse = "a" -- Enable mouse
 set.ignorecase = false
 set.smartcase = false
 set.hlsearch = true -- Highlight search
@@ -19,8 +19,8 @@ set.tabstop = 2 -- Use a tab width of 2 spaces
 set.softtabstop = 2
 set.shiftwidth = 2
 set.expandtab = false
-set.background = 'dark'
-set.cursorline = true  -- Highlight current line with cursor
+set.background = "dark"
+set.cursorline = true -- Highlight current line with cursor
 set.autoindent = true -- Enable autoindent
 -- set.re = 2
 set.ruler = true
@@ -31,22 +31,21 @@ set.confirm = true -- Prompts user on unsaved edited file when exiting
 set.wildmenu = true
 -- set.visualbell = true
 set.scrolloff = 5 -- Keep 5 lines above and below cursor
-set.nrformats = 'bin' -- Use binary increment and decrement
-set.encoding = 'utf-8'
+set.nrformats = "bin" -- Use binary increment and decrement
+set.encoding = "utf-8"
 set.redrawtime = 800
 set.undofile = true -- Enable persistent undo
-set.omnifunc = 'syntaxcomplete#Complete' -- Enable Neovim completion
-set.colorcolumn = '100,120' -- Character width ruler
-set.signcolumn = 'yes'
+set.omnifunc = "syntaxcomplete#Complete" -- Enable Neovim completion
+set.colorcolumn = "80,120" -- Character width ruler
+set.signcolumn = "yes"
 -- set.timeoutlen = 1000
 -- set.updatetime = 1000
-set.undodir = vim.fn.expand('~/.cache/nvim-undo') -- Set undo directory
-set.completeopt = 'menu,menuone,noinsert,noselect' -- Don't select first item in predictions
+set.undodir = vim.fn.expand("~/.cache/nvim-undo") -- Set undo directory
+set.completeopt = "menu,menuone,noinsert,noselect" -- Don't select first item in predictions
 set.list = false
-g.mapleader = ' ' --Set Space Key as map leader
+g.mapleader = " " --Set Space Key as map leader
 g.rustfmt_autosave = 1 -- Format rust code on save
 
-if vim.fn.has('termguicolors') == 1 then -- Enable termguicolors if available
+if vim.fn.has("termguicolors") == 1 then -- Enable termguicolors if available
 	set.termguicolors = true
 end
-
