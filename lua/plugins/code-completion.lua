c442899046207b18d0f67d31e197b1ef4f17592b
xirlorm <xirlorm@gmail.com>
2026-05-27 05:15:11 +0000

    Unified code completion configuration

--- lua/plugins/code-completion.lua
+++ lua/plugins/code-completion.lua
@@ -1,10 +1,83 @@
 return {
-	"neovim/nvim-lspconfig",
+	"hrsh7th/nvim-cmp",
+	event = "VeryLazy",
 	dependencies = {
+		"neovim/nvim-lspconfig",
 		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
 	},
 	config = function()
-		-- Setup language servers.
+		-- ########################################
+		-- #        Autocompletion setup          #
+		-- ########################################
+		local luasnip = require("luasnip")
+		local cmp = require("cmp")
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
+
+		-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+		-- Set configuration for specific filetype.
+		--[[	cmp.setup.filetype("gitcommit", {
+			sources = cmp.config.sources({
+				{ name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+			}, {
+				{ name = "buffer" },
+			})
+		})
+		require("cmp_git").setup() ]]
+
+		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+			matching = { disallow_symbol_nonprefix_matching = false },
+		})
+
+		-- Use cmdline & path source for ":" (if you enabled `native_menu`, this won"t work anymore).
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+		})
+
+		-- ########################################
+		-- #  Set up lspconfig language servers   #
+		-- ########################################
 		local lspconfig = vim.lsp.config
 
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -46,10 +119,6 @@ return {
 		})
 
 		-- Rust analyzer configuration
-		-- local on_attach = function(client)
-		-- 	require'completion'.on_attach(client)
-		-- end
-
 		lspconfig("rust_analyzer", {
 			-- on_attach = on_attach,
 			cmd = { "rust-analyzer" },
@@ -67,25 +136,25 @@ return {
 		})
 		vim.lsp.enable("rust_analyzer")
 
-		-- -- Dart lsp configuration
-		-- lspconfig('dartls', {
-		-- 	capabilities = capabilities,
-		-- 	cmd = { '/home/sailor/Apps/flutter/bin/dart', 'language-server', '--protocol=lsp' },
-		-- 	filetypes = { 'dart' },
-		-- 	settings = {
-		-- 		dart = {
-		-- 			completeFunctionCalls = true,
-		-- 			showTodos = true,
-		-- 		},
-		-- 	},
-		-- 	init_options = {
-		-- 		closingLabels = true,
-		-- 		flutterOutline = true,
-		-- 		onlyAnalyzeProjectsWithOpenFiles = true,
-		-- 		outline = true,
-		-- 		suggestFromUnimportedLibraries = true
-		-- 	}
-		-- })
+		-- Dart lsp configuration
+		lspconfig("dartls", {
+			capabilities = capabilities,
+			cmd = { "/home/sailor/Apps/flutter/bin/dart", "language-server", "--protocol=lsp" },
+			filetypes = { "dart" },
+			settings = {
+				dart = {
+					completeFunctionCalls = true,
+					showTodos = true,
+				},
+			},
+			init_options = {
+				closingLabels = true,
+				flutterOutline = true,
+				onlyAnalyzeProjectsWithOpenFiles = true,
+				outline = true,
+				suggestFromUnimportedLibraries = true,
+			},
+		})
 
 		-- Global mappings.
 		-- See `:help vim.diagnostic.*` for documentation on any of the below functions
