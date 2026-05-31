43878b8b0225642b4b99a8b5c1756dc57fec530f
xirlorm <xirlorm@gmail.com>
2026-05-31 19:44:29 +0000

    Added color picker plugin

---
+++ lua/plugins/color-picker.lua
@@ -0,0 +1,14 @@
+return {
+	"uga-rosa/ccc.nvim",
+	config = function()
+		local ccc = require("ccc")
+
+		ccc.setup({
+			highlighter = {
+				auto_enable = true,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>cp", "<cmd>CccPick<CR>")
+	end,
+}
